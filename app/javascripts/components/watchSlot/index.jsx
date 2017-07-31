0b915a110693f1829cb98e905e39499e1ff8b2eb
c0smic0wl <c0smic0wl@outlook.com>
2017-07-31 21:57:22 +0900

    Make watch page and modify links

---
+++ app/javascripts/components/watchSlot/index.jsx
@@ -0,0 +1,147 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import SlotGame from '../playSlot/game';
+import * as Actions from './actions';
+import Web3Service from '../../helpers/web3Service';
+import styles from './watchSlot.scss';
+
+let gameAlreadyLoaded = false;
+let slotMachineLoaded = false;
+
+function mapStateToProps(appState) {
+  return {
+    root: appState.root,
+    watchSlotState: appState.watchSlot,
+  };
+}
+
+class WatchSlot extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.slotAddress = this.props.match.params.slotAddress;
+    Web3Service.createGenesisRandomNumber(this.slotAddress);
+  }
+
+  componentDidMount() {
+    if (!this.canvas || gameAlreadyLoaded) {
+      return;
+    }
+
+    const { root, watchSlotState } = this.props;
+
+    gameAlreadyLoaded = true;
+    if (root.get('account') !== null && !slotMachineLoaded) {
+      this.getSlotMachine(this.slotAddress, root.get('account'));
+      slotMachineLoaded = true;
+    }
+
+    this.slotGame = new SlotGame({
+      canvas: this.canvas,
+      isLoading: watchSlotState.get('isLoading'),
+      hasError: watchSlotState.get('hasError'),
+      lineNum: watchSlotState.get('lineNum'),
+      betSize: watchSlotState.get('betSize'),
+      betUnit: watchSlotState.get('betUnit'),
+      minBet: watchSlotState.get('minBet'),
+      maxBet: watchSlotState.get('maxBet'),
+      bankRoll: Web3Service.makeEthFromWei(watchSlotState.get('bankRoll')),
+      yourStake: Web3Service.makeEthFromWei(watchSlotState.get('deposit')),
+      playGame: this.playGame.bind(this),
+      isBanker: true,
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { root, watchSlotState } = nextProps;
+
+    if (this.props.watchSlotState !== watchSlotState || this.props.root !== root) {
+      this.slotGame.isLoading = watchSlotState.get('isLoading');
+      this.slotGame.lineNum = watchSlotState.get('lineNum');
+      this.slotGame.betSize = watchSlotState.get('betSize');
+      this.slotGame.betUnit = watchSlotState.get('betUnit');
+      this.slotGame.maxBet = watchSlotState.get('maxBet');
+      this.slotGame.minBet = watchSlotState.get('minBet');
+      this.slotGame.hasError = watchSlotState.get('hasError');
+      this.slotGame.bankRoll = Web3Service.makeEthFromWei(watchSlotState.get('bankRoll'));
+      this.slotGame.yourStake = Web3Service.makeEthFromWei(watchSlotState.get('deposit'));
+
+      if (watchSlotState.get('hasError')) {
+        this.slotGame.errorOccur();
+      }
+
+      if (root.get('account') !== null && !slotMachineLoaded) {
+        this.getSlotMachine(this.slotAddress, root.get('account'));
+        slotMachineLoaded = true;
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.slotGame) {
+      this.slotGame.removeCurrentGame();
+      gameAlreadyLoaded = false;
+    }
+  }
+
+  render() {
+    const { watchSlotState } = this.props;
+
+    return (
+      <div className={styles.watchSlotSection}>
+        <div className={styles.watchSlotContainer}>
+          <div className={styles.innerHeader}>
+            <div className={styles.slotName}>Slot Name</div>
+            <div className={styles.rightBtns}>
+              <button
+                className={styles.helpBtn}
+                onClick={() => {
+                  alert('help');
+                }}
+              >
+                ?
+              </button>
+              <button onClick={this.setDeposit} className={styles.headerBtn}>
+                KICK
+              </button>
+              <button onClick={this.leaveSlotMachine} className={styles.headerBtn}>
+                CASH OUT
+              </button>
+            </div>
+          </div>
+          <div className={styles.gameContainer}>
+            <canvas
+              ref={canvas => {
+                this.canvas = canvas;
+              }}
+            />
+          </div>
+        </div>
+        <div className={styles.bottomSection}> bottom</div>
+      </div>
+    );
+  }
+
+  playGame() {
+    const { dispatch, root, watchSlotState } = this.props;
+    const gameInfo = {
+      slotMachineContract: watchSlotState.get('slotMachineContract'),
+      playerAddress: root.get('account'),
+      betSize: watchSlotState.get('betSize'),
+      lineNum: watchSlotState.get('lineNum'),
+    };
+    dispatch(Actions.requestToPlayGame(gameInfo, this.slotGame.stopSpin));
+  }
+
+  getSlotMachine(slotAddress, playerAddress) {
+    const { dispatch } = this.props;
+    dispatch(Actions.getSlotMachine(slotAddress, playerAddress));
+  }
+
+  leaveSlotMachine() {
+    const { dispatch, root, watchSlotState } = this.props;
+    dispatch(Actions.leaveSlotMachine(watchSlotState.get('slotMachineContract'), root.get('account')));
+  }
+}
+
+export default connect(mapStateToProps)(WatchSlot);
---
+++ app/javascripts/components/watchSlot/reducer.jsx
@@ -0,0 +1,129 @@
+import { fromJS } from 'immutable';
+import { ACTION_TYPES } from './actions';
+import Web3Service from '../../helpers/web3Service';
+
+export const WATCH_SLOT_INITIAL_STATE = fromJS({
+  isLoading: false,
+  isPlaying: false,
+  isOccupied: false,
+  hasError: false,
+  betSize: 20,
+  lineNum: 20,
+  deposit: 0,
+  bankRoll: 0,
+  betUnit: 2,
+  minBet: 2,
+  maxBet: 20,
+  slotMachineContract: null,
+});
+
+export function reducer(state = WATCH_SLOT_INITIAL_STATE, action) {
+  switch (action.type) {
+    case ACTION_TYPES.START_TO_SEND_ETHER_TO_CONTRACT:
+    case ACTION_TYPES.START_TO_GET_SLOT_MACHINE: {
+      return state.withMutations(currentState => {
+        return currentState.set('isLoading', true).set('hasError', false);
+      });
+    }
+
+    case ACTION_TYPES.FAILED_TO_GET_SLOT_MACHINE: {
+      return state.withMutations(currentState => {
+        return currentState.set('isLoading', false).set('hasError', true);
+      });
+    }
+
+    case ACTION_TYPES.SET_DEPOSIT: {
+      return state.set('deposit', action.payload.weiValue);
+    }
+
+    case ACTION_TYPES.SET_OCCUPIED_STATE: {
+      return state.set('isOccupied', action.payload.occupied);
+    }
+
+    case ACTION_TYPES.SUCCEEDED_TO_GET_SLOT_MACHINE: {
+      return state.withMutations(currentState => {
+        return currentState
+          .set('isLoading', false)
+          .set('hasError', false)
+          .set('minBet', parseFloat(action.payload.minBet))
+          .set('betSize', parseFloat(action.payload.minBet))
+          .set('maxBet', parseFloat(action.payload.maxBet))
+          .set('betUnit', parseFloat(action.payload.minBet))
+          .set('bankRoll', action.payload.bankRoll) // Big Number
+          .set('deposit', action.payload.deposit) // Big Number
+          .set('slotMachineContract', action.slotMachineContract);
+      });
+    }
+
+    case ACTION_TYPES.SEND_ETHER_TO_SLOT_CONTRACT: {
+      return state.withMutations(currentState => {
+        const bigNumber = Web3Service.getWeb3().toBigNumber(
+          parseFloat(action.payload.weiValue, 10) + parseFloat(currentState.get('deposit'), 10),
+        );
+        return currentState.set('deposit', bigNumber).set('isLoading', false).set('hasError', false);
+      });
+    }
+
+    case ACTION_TYPES.START_TO_OCCUPY_SLOT_MACHINE: {
+      return state.withMutations(currentState => {
+        return currentState.set('isLoading', true).set('hasError', false);
+      });
+    }
+
+    case ACTION_TYPES.FAILED_TO_SEND_ETHER_TO_CONTRACT:
+    case ACTION_TYPES.FAILED_TO_OCCUPY_SLOT_MACHINE: {
+      return state.withMutations(currentState => {
+        return currentState.set('isLoading', false).set('hasError', true);
+      });
+    }
+
+    case ACTION_TYPES.SUCCEEDED_TO_OCCUPY_SLOT_MACHINE: {
+      return state.withMutations(currentState => {
+        return currentState.set('isLoading', false).set('hasError', false).set('isOccupied', true);
+      });
+    }
+
+    case ACTION_TYPES.START_TO_PLAY_GAME: {
+      return state.withMutations(currentState => {
+        return currentState.set('isPlaying', true).set('hasError', false);
+      });
+    }
+
+    case ACTION_TYPES.FAILED_TO_PLAY_GAME: {
+      return state.set('hasError', true);
+    }
+
+    case ACTION_TYPES.SUCCEEDED_TO_PLAY_GAME: {
+      return state.withMutations(currentState => {
+        return currentState
+          .set('isPlaying', false)
+          .set('deposit', currentState.get('deposit').plus(parseFloat(action.payload.diffMoney, 10)))
+          .set('bankRoll', currentState.get('bankRoll').minus(parseFloat(action.payload.diffMoney, 10)))
+          .update('betsData', list => list.concat(action.payload.transaction.betData));
+      });
+    }
+
+    case ACTION_TYPES.SET_BET_SIZE: {
+      return state.set('betSize', action.payload.betSize);
+    }
+
+    case ACTION_TYPES.SET_LINE_NUM: {
+      return state.set('lineNum', action.payload.lineNum);
+    }
+
+    case ACTION_TYPES.SET_BANK_ROLL: {
+      return state.set('bankRoll', action.payload.bankRoll);
+    }
+
+    case ACTION_TYPES.SPIN_START: {
+      return state.set('isSpinning', true);
+    }
+
+    case ACTION_TYPES.SPIN_END: {
+      return state.set('isSpinning', false);
+    }
+
+    default:
+      return state;
+  }
+}
